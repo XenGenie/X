947a2a92a5c4e724b26b1ba07b58b0a266987f1e
xopherdeep <heylisten@xtiv.net>
2014-07-17 07:37:59 -0700

    updating submodule init process

--- gitsub.sh
+++ gitsub.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+git submodule init
+git submodule update
+
 for x in $(ls -d */)
 do 
 	pattern='^.*[^/]'    #Regular expressions are "greedy", they'll match the longest substring they can from the 
 	[[ $x =~ $pattern ]]   #Apply regex
 	x=${BASH_REMATCH[0]}
-	printf "Remove  %s\n" $x
-	git rm -rf --cached $x
-	rm $x -r
-	printf "Adding submodule %s .git\n" $x
-	git submodule add -f https://github.com/SuperDomX/$x $x
+
+	# printf "Remove  %s\n" $x
+	# git rm -rf --cached $x
+	# rm $x -r
+	# printf "Adding submodule %s .git\n" $x
+
+	# git submodule add -f https://github.com/SuperDomX/$x $x
 
 	cd $x
 	git submodule init
