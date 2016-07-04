1ca1a9ba15e94dfda316ff7b61a3a8f4440f8e7b
xopherdeep <heylisten@xtiv.net>
2016-07-04 02:30:07 -0700

    sync

--- gitpush.sh
+++ gitpush.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for x in $(ls -d */)
-do 
-    echo "Nightly Push of $x"
-    cd $x
-    git pull origin master
-    git add .
-    git commit -a -m "$x Automated Nightly Push"
-    git push origin +HEAD:refs/heads/master
-    cd ..
-done 
+do
+  echo "Nightly Push of $x"
+  cd $x
+  git pull origin master
+  git add .
+  git commit -a -m "$x Automated Nightly Push"
+  git push origin +HEAD:refs/heads/master
+  cd ..
+done
