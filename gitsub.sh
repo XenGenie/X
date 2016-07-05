13bb2ab3e633890548d28894a9b9d0cbecbf4ea8
xopherdeep <heylisten@xtiv.net>
2016-07-04 23:11:23 -0700

    update modules

--- gitsub.sh
+++ gitsub.sh
@@ -3,7 +3,7 @@ git submodule init
 git submodule update
 
 for x in $(ls -d */)
-do 
+do
 	pattern='^.*[^/]'    #Regular expressions are "greedy", they'll match the longest substring they can from the 
 	[[ $x =~ $pattern ]]   #Apply regex
 	x=${BASH_REMATCH[0]}
@@ -19,5 +19,4 @@ do
 	git submodule init
 	git submodule update
 	cd ..
- 
 done
