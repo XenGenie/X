7ea601550b9519fa1dc804963f684f37cab3f053
xopherdeep <heylisten@xtiv.net>
2016-07-10 15:25:06 -0700

    update modules

--- gitpush.sh
+++ gitpush.sh
@@ -2,13 +2,12 @@
 max_processes=30
 for x in $(ls -d */)
 do
-  echo "Nightly Push of $x " &
-  cd $x &
-  git pull origin master &
-  git add . &
-  git commit -a -m "$x Automated Nightly Push" &
-  git push origin +HEAD:refs/heads/master &
-  cd .. &
+  echo "Nightly Push of $x " 
+  cd $x 
+  git pull origin master 
+  git add . 
+  git commit -a -m "$x Automated Nightly Push" 
+  git push origin +HEAD:refs/heads/master 
+  cd .. 
 done
 
-echo "Done"
